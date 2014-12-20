5e0bcf89c5944d5cb66b004e837a51eb16048491
Stewart Newnham <git@newnham.com>
2014-12-21 03:17:15 +0800

    Commit 6: Add Contsructors

--- lLecture10/src/qu1_4/OldPhoneImpl.java
+++ lLecture10/src/qu1_4/OldPhoneImpl.java
@@ -2,6 +2,18 @@ package qu1_4;
 
 public class OldPhoneImpl implements OldPhone {
 
+	private String brand;
+	
+	public OldPhoneImpl(String brand) {
+		this.brand = brand; 
+	}	
+	
+	public void getBrand() { 
+		// return brand;
+		System.out.println("Phone Brand: " + brand);
+	}
+		
+	
 	@Override
 	public void call(String number) {
 		System.out.println("Calling: " + number);
