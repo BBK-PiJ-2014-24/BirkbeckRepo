f58d516822129298c7334733a6660dd098bcbbb6
Stewart Newnham <git@newnham.com>
2014-12-21 03:10:59 +0800

    Commit5: Improve SmartPhone

--- lLecture10/src/qu1_4/PhoneTest.java
+++ lLecture10/src/qu1_4/PhoneTest.java
@@ -3,7 +3,11 @@ package qu1_4;
 public class PhoneTest {
 
 	public static void main(String[] args) {
-		
+		PhoneTest p = new PhoneTest();
+		p.launch();
+	}
+
+	public void launch(){
 		System.out.println("OldPhone Test");
 		OldPhone o = new OldPhoneImpl();
 		o.call("111-1111");
@@ -18,7 +22,8 @@ public class PhoneTest {
 		SmartPhone s = new SmartPhone();
 		s.browseWeb("Amazon.com");
 		s.findPoistion();
-
-	}
-
+		s.call("222-2222");
+		s.call("00-333-3333");
+		}
+	
 }
--- lLecture10/src/qu1_4/SmartPhone.java
+++ lLecture10/src/qu1_4/SmartPhone.java
@@ -2,6 +2,16 @@ package qu1_4;
 
 public class SmartPhone extends MobilePhone {
 	
+	@Override
+	public void call(String number){
+		String start = number.substring(0,2);
+		if(start.equals("00"))
+			System.out.println("Calling " + number + " through the internet to save money");
+		else
+			super.call(number);
+	}
+	
+	
 	public void browseWeb(String website){
 		System.out.println("Surfing Web to Find: " + website);
 	}
