aa3d3cdeaec23237c7539a050b9a318a6923ff3b
Stewart Newnham <git@newnham.com>
2014-12-21 02:44:37 +0800

    Commit3: MobilePhone Class and Test

---
+++ lLecture10/src/qu1_4/MobilePhone.java
@@ -0,0 +1,13 @@
+package qu1_4;
+
+public class MobilePhone extends OldPhoneImpl{
+
+	public void ringAlarm(){
+		System.out.println("Beep! Beep! Beep!");
+	}
+	
+	
+	public void playGame(String game){
+		System.out.println("playing Game: " + game);
+	}
+}
--- lLecture10/src/qu1_4/PhoneTest.java
+++ lLecture10/src/qu1_4/PhoneTest.java
@@ -4,8 +4,15 @@ public class PhoneTest {
 
 	public static void main(String[] args) {
 		
+		System.out.println("OldPhone Test");
 		OldPhone o = new OldPhoneImpl();
 		o.call("111-1111");
+		
+		System.out.println("\nMobilePhone Test");
+		MobilePhone m = new MobilePhone();
+		m.call("111-1111");
+		m.ringAlarm();
+		m.playGame("space invaders");
 
 	}
 
