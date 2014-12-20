bd2f269e7699e782b865de0ee94e59e030d8c196
Stewart Newnham <git@newnham.com>
2014-12-21 02:20:58 +0800

    First Commit of Lecture10 Project. Old Phone Implementation

---
+++ lLecture10/src/qu1_4/OldPhone.java
@@ -0,0 +1,10 @@
+package qu1_4;
+
+public interface OldPhone {
+	/**
+	* A phone makes calls 
+	* Just print on the screen: "Calling <number>...".
+	*/
+	void call(String number);
+	}
+
---
+++ lLecture10/src/qu1_4/OldPhoneImpl.java
@@ -0,0 +1,10 @@
+package qu1_4;
+
+public class OldPhoneImpl implements OldPhone {
+
+	@Override
+	public void call(String number) {
+		System.out.println("Calling " + number);
+	}
+
+} // end class
