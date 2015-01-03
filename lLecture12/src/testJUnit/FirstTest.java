b5312b4953119c58cd39059f68455ce19e606c56
Stewart Newnham <git@newnham.com>
2015-01-03 22:25:22 +0800

    Testing the before() test Unit

--- lLecture12/src/testJUnit/FirstTest.java
+++ lLecture12/src/testJUnit/FirstTest.java
@@ -2,15 +2,24 @@ package testJUnit;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FirstTest {
 
+	@Before
+	public void initiateClass(){
+		System.out.println("Test Before");
+	}
+	
 	@Test
 	public void testMultiply() {
 		TestMethods m = new TestMethods();
-		int result = m.multiply(5, 6);
-		assertEquals(30, result);
+		int result1 = m.multiply(5, 6);
+		int result2 = m.multiply(-1, 2);
+		assertEquals("5x6", 30, result1);
+		assertEquals("-1*2",-2,result2);
+		
 	}
 
 }
