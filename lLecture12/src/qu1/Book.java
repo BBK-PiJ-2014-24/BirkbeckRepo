0373710129b60c54bf39bcc3df3f5d2e1c82f0e7
Stewart Newnham <git@newnham.com>
2015-01-06 04:46:56 +0800

    Extended Book Class for setBorrowed() and isBorrowed() methods.

--- lLecture12/src/qu1/Book.java
+++ lLecture12/src/qu1/Book.java
@@ -8,6 +8,7 @@ public class Book {
 	// ------
 	private String title;
 	private String author;
+	private boolean borrowed;
 	
 	/**
 	 * Constructor: BOOK
@@ -19,6 +20,7 @@ public class Book {
 	public Book(String t, String a){
 		title = t;
 		author = a;
+		borrowed = false;
 	}
 	
 	/**
@@ -37,8 +39,23 @@ public class Book {
 	}
 	
 
-	
-	
+	/**
+	 * isBorrowed() Determines if the book is Taken Out. 
+	 * @return returns true if book is on Loan (borrowed); returns false if book is not taken out.
+	 */
+	public boolean isBorrowed(){
+		if(borrowed=false)
+			return false;
+		else
+			return true;
+	}
+	/**
+	 * setBorrowed - setter method for flagging book is borrowed or Not.
+	 * @param isBorrowed
+	 */
+	public void setBorrowed(boolean isBorrowed){
+		borrowed = isBorrowed;
+	}
 	
 	
 }
--- lLecture12/src/qu1_Test/BookTest.java
+++ lLecture12/src/qu1_Test/BookTest.java
@@ -8,14 +8,19 @@ import qu1.Book;
 public class BookTest {
 
 	@Test
-	public void test() {
-		
+	public void test1() {
 		Book b = new Book("The Power of Now", "Tolle");
 		String title = b.getTitle();
 		String author = b.getAuthor();
 		assertEquals("getTitle Check", "The Power of Now", title);
 		assertEquals("getAuthor Check", "Tolle", author );
-
+	}
+	
+	@Test
+	public void test2(){
+		Book b = new Book("The Power of Now", "Tolle");
+		b.setBorrowed(true);
+		assertTrue("Test BookBorrowing Methods",b.isBorrowed());
 	}
 
 }
