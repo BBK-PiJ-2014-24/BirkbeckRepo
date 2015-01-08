8b8b7dd6192a007c75bcde19c4d71f17370ef8fd
Stewart Newnham <git@newnham.com>
2015-01-09 04:15:40 +0800

    Refactored returnBook() in BookUserClass

--- lLecture12/src/qu1_Test/LibraryUserTest.java
+++ lLecture12/src/qu1_Test/LibraryUserTest.java
@@ -2,6 +2,9 @@ package qu1_Test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import qu1.LibraryImpl;
@@ -31,8 +34,8 @@ public class LibraryUserTest {
 	
 	@Test  // Register Checks On UserName and Library Classes 
 	public void test2(){
-		
 		LibraryImpl l = new LibraryImpl("Croydon");
+		l.setMaxBookPerUser(3);
 		LibUser u = new LibUserImpl("Stew");
 		u.register(l);
 		assertEquals("UserName Class Register Check", "Croydon",u.getLibrary().getLibraryName());
@@ -40,6 +43,68 @@ public class LibraryUserTest {
 	}
 	
 	
+	// getBorrowedBookTest()
+	@Test
+	public void test3(){
+		LibraryImpl l = new LibraryImpl("Croydon");
+		l.setMaxBookPerUser(3);
+		LibUser u = new LibUserImpl("Stew");
+		u.register(l);
+		l.addBook("The Power of Now", "Tolle");  // populate Library with books
+		l.addBook("AntiFragile", "Taleb");
+		l.addBook("GoldFinger", "Ian Fleming");
+		l.addBook("MoonRaker", "Ian Fleming");		
+		
+		List<String> testArray = new ArrayList<String>(); 
+		List<String> z = u.getBorrowedBookList();
+		assertEquals("Test user's BorrowedBook List is Empty Before populated: ", testArray, z);
+		
+		u.borrowBook("The Power of Now");  // user borrows two books
+		u.borrowBook("AntiFragile");
+		
+		testArray.add("The Power of Now"); // populate Dummy List
+		testArray.add("AntiFragile");
+		
+		z = u.getBorrowedBookList();
+		assertEquals("Test user's BorrowedBook List populated with the two books: ", testArray, z);
+		
+		u.borrowBook("Gone With the Wind");
+		z = u.getBorrowedBookList();
+		assertEquals("Test user's BorrowedBook List After Invalid Book Request: ", testArray, z);
+		
+		u.borrowBook("GoldFinger");
+		u.borrowBook("MoonRaker");
+		testArray.add("GoldFinger"); // Further populate Dummy List
+		z = u.getBorrowedBookList();
+		assertEquals("Test user's BorrowedBook List After Attempt to Breach Max Book on Loan: ", testArray, z);
+		
+	}
 	
+	// test returnBook()
+	@Test
+	public void test4(){
+		LibraryImpl l = new LibraryImpl("Croydon");
+		l.setMaxBookPerUser(3);
+		LibUser u = new LibUserImpl("Stew");
+		u.register(l);
+		l.addBook("The Power of Now", "Tolle");  // populate Library with books
+		l.addBook("AntiFragile", "Taleb");
+		l.addBook("GoldFinger", "Ian Fleming");
+		l.addBook("MoonRaker", "Ian Fleming");
+		
+		u.borrowBook("The Power of Now");  // user borrows two books
+		u.borrowBook("AntiFragile");
+		
+		u.returnBook("The Power of Now");
+		
+		List<String> testArray = new ArrayList<String>(); 
+		testArray.add("AntiFragile"); // populate Dummy List
+		List<String> z = u.getBorrowedBookList();
+		assertEquals("Test user's returnBook by checking user's borrowed book list: ", testArray, z);
+		
+		
+		u.returnBook("Gone with the Wind");
+		u.returnBook("MoonRaker");
+	}
 
 }
