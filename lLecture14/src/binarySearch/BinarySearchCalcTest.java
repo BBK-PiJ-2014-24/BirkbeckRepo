4623f9b0768497a3aafaa76ee7fd1670505dbc3f
Stewart Newnham <git@newnham.com>
2015-01-12 20:00:37 +0800

    Added Further Tests for Bin Search.

--- lLecture14/src/binarySearch/BinarySearchCalcTest.java
+++ lLecture14/src/binarySearch/BinarySearchCalcTest.java
@@ -15,15 +15,26 @@ public class BinarySearchCalcTest {
 		for(int i=0; i<100; i++) {
 			list.add(i);
 		}
+		
 		System.out.println(list.toString());
 		
 		int x = 15;
 		int y = 102;
-		boolean isX = BinarySearchCalc(x);
-		boolean isY = BinarySearchCalc(y);
+		int m = 50;
+		int startArray = 0;
+		int endArray = 99;
+		
+		boolean isX = BinarySearchCalc(x, list);
+		boolean isY = BinarySearchCalc(y, list);
+		boolean isM = BinarySearchCalc(m, list);
+		boolean isStartArray = BinarySearchCalc(startArray, list);
+		boolean isEndArray = BinarySearchCalc(endArray, list);
 		
 		assertEquals("Test Bin Search when number x is in Array", true, isX);
-		assertEquals("Test Bin Search when number  y is NOT in Array", false, isY);
+		assertEquals("Test Bin Search when number y is NOT in Array", false, isY);
+		assertEquals("Test Bin Search when Middle number m is in Array", true, isM);
+		assertEquals("Test Bin Search when Start number is in Array", true, isStartArray);
+		assertEquals("Test Bin Search when end number is in Array", true, isEndArray);
 	}
 
 }
