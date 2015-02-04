ea0d778f611731f6b4f0d98e92df29fbfbf1c909
Stewart Newnham <git@newnham.com>
2015-02-04 19:11:41 +0800

    Tweaked to add a Future Return Variable

--- lLecture18/src/myFirstPool/StartPool.java
+++ lLecture18/src/myFirstPool/StartPool.java
@@ -3,6 +3,7 @@ package myFirstPool;
 
 import java.util.concurrent.ExecutorService;   // An Interface
 import java.util.concurrent.Executors;         // An Implementation of the Interface
+import java.util.concurrent.Future;
 
 public class StartPool {
 
@@ -11,6 +12,9 @@ public class StartPool {
 		pool.launch();
 	}
 	
+	
+	
+	
 	public void launch(){
 		
 		ExecutorService exec = Executors.newFixedThreadPool(2);       // Instant ExecutorService
@@ -18,11 +22,13 @@ public class StartPool {
 		for(int i=0;i<5;i++){
 			
 			Runnable Toji = new TaskToDo(i);  				// Instant Runnable
-			exec.submit(Toji);								// submit on a runnable
-			//exec.submit(new TaskToDo(i));					// Done in One Step
+			// exec.submit(Toji);							// Simple submit()
+			//exec.submit(new TaskToDo(i));					// Runnable() + Submit() Done in One Step
+			
+			Future fut = exec.submit(Toji);					// Return a Future on a submit
+			System.out.println("Is Thread " + i + " done? " + fut.isDone()); // Simple use of a sumbit()
 		}
-		
-		
+		exec.shutdown();
 	}
 
 }
